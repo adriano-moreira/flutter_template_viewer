c05420211e5fdd8195e2fb2002c7f4b55fe10d6f
Adriano Moreira <adri3d@gmail.com>
2020-08-18 16:40:23 -0300

    clean

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_template_viewer/flutter_template_viewer.dart';
 
 void main() {
-  runApp(AppDemo());
+  runApp(
+    AppDemo(),
+  );
 }
 
 class MyTemplate extends StatelessWidget {
   final Color color;
 
-  MyTemplate({this.color = Colors.deepPurpleAccent});
+  MyTemplate({
+    this.color = Colors.deepPurpleAccent,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -40,27 +44,48 @@ class AppDemo extends StatelessWidget {
         body: TemplateViewer(
           templates: [
             TemplateData(
-                name: 'Flutter Logo', templateBuilder: (ctx) => FlutterLogo()),
+                name: 'Flutter Logo',
+                templateBuilder: (ctx) => Container(
+                      color: Colors.amberAccent,
+                      child: Center(
+                        child: SizedBox(
+                          width: 320,
+                          height: 320,
+                          child: Container(
+                            color: Colors.white,
+                            child: FlutterLogo(),
+                          ),
+                        ),
+                      ),
+                    )),
             TemplateData(
               name: 'Placeholder',
               templateBuilder: (ctx) => Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Center(
-                      child: SizedBox(
-                        height: 360,
-                        width: 360,
-                        child: Text('Text much much text long long long', style: Theme.of(context).textTheme.headline2,),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Row(
+                          children: [
+                            Text(
+                              'Text very long',
+                              style: Theme.of(context).textTheme.headline2,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20,),
-                    RaisedButton(
-                      onPressed: () {},
-                      child: Text('Button'),
-                      color: Theme.of(context).primaryColor,
-                    )
-                  ],
+                      SizedBox(
+                        height: 20,
+                      ),
+                      RaisedButton(
+                        onPressed: () {},
+                        child: Text('Button'),
+                        color: Theme.of(context).primaryColor,
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
